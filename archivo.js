f52da0c04802884c1ba469387873064c8493b529
Julieta <109936071+Julis19@users.noreply.github.com>
2023-08-27 16:33:21 -0300

    Add files via upload

---
+++ archivo.js
@@ -0,0 +1,10 @@
+let nombre="Maria";//string
+let apellido="Bolena";//string
+console.log(nombre+""+apellido)
+
+let numberone=120;
+let numbertwo=20;
+let suma = numberone + numbertwo;
+console.log("Suma:";resultsuma);
+
+
